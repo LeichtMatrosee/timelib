4748b84a5c2c47142e36328e6bcdbcfe2f65bcd0
Gerrit Koppe <gkoppe@sievers-group.com>
2022-03-29 17:41:14 +0200

    Declared functions and commented

--- main.c
+++ main.c
@@ -1,24 +1,80 @@
 /**
- *
- *
- *
- **/
-
+ * @file main.c
+ * @author Gerrit Koppe
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- *
- *
- **/
-int day_of_the_year(int day, int month, int year)
-{
+ * @brief Leert den Eingabebuffer
+ * 
+ * @return int 
+ */
+int clearBuffer() {
+    while (getchar() != '\n') {
+        continue;
+    };
+    return 0;
+}
+
+void input_date(day, month, year) {
+
+}
+
+/**
+ * @brief Überprüft, ob ein Jahr ein Schaltjahr ist
+ * 
+ * @param year Eingegebenes Jahr
+ * @return int 1, falls das Jahr ein Schaltjahr ist; 0, falls das Jahr kein Schaltjahr ist.
+ */
+int is_leapyear(int year) {
+
+}
+
+/**
+ * @brief Get the days for month object
+ * 
+ * @param month Eingegebener Monat
+ * @param year Eingegebenes Jahr
+ * @return int Anzahl der Tage im eingegebenen Monat und -1, falls ein ungültiger Monat oder ein ungültiges
+ * Jahr übergeben wurde.
+ */
+int get_days_for_month(int month, int year) {
+
+}
+
+/**
+ * @brief Überprüft, ob das angegebene Datum existiert.
+ * 
+ * @param day Eingegebener Tag
+ * @param month Eingegebener Monat
+ * @param year Eingegebenes Jahr
+ * @return int 1, falls es existiert; 0, falls es nicht existiert
+ */
+int exists_date(int day, int month, int year) {
+
+}
+
+/**
+ * @brief Berechnet, der wie vielte Tag des Jahres der angegebene Tag ist.
+ * 
+ * @param day Eingegebener Tag
+ * @param month Eingegebener Monat
+ * @param year Eingegebenes Jahr
+ * @return int Ergebnis der Berechnung
+ */
+int day_of_the_year(int day, int month, int year) {
 
     return 0;
 }
 
-int main()
-{
+int main() {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
     return 0;
 }
